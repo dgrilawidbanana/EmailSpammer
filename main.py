6c2190cd3024892ae16fcbf4b1d32d0011c7f29e
Eric Wang <93156719+theUnknowableOne@users.noreply.github.com>
2022-01-10 11:26:07 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-choice=input("CHOOSE VERSION (SEE FILES IF UNSURE):\n[1] (NEWEST VERION, RECOMMENDED) OR [2]\n>>> ")
-if choice=="1":
+choice = input("CHOOSE VERSION (SEE FILES IF UNSURE):\n[1] (NEWEST VERION, RECOMMENDED) OR [2]\n>>> ")
+if choice == "1":
   exec(open("./MainVersion.py").read())
-elif choice=="2":
+elif choice == "2":
   exec(open("./AlternativeVersion.py").read())
