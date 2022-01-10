adb25891b2d9b2a7f0310513d5c32248f1dbc0e9
Eric Wang <93156719+theUnknowableOne@users.noreply.github.com>
2022-01-10 11:18:20 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+choice=input("CHOOSE VERSION (SEE FILES IF UNSURE):\n[1] (NEWEST VERION, RECOMMENDED) OR [2]\n>>> ")
+if choice=="1":
+  exec(open("./MainVersion.py").read())
+elif choice=="2":
+  exec(open("./AlternativeVersion.py").read())
